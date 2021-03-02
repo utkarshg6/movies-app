54c9ee8ac1ccc65f4baf40732a7629c9b450c424
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-03-02 09:28:05 +0000

    Add Dropdown Search

--- components/menu-bar.js
+++ components/menu-bar.js
@@ -1,29 +1,94 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Menu, Search } from 'semantic-ui-react';
 import { Link } from '../routes';
+import _ from 'lodash';
 
-const MenuBar = () => {
-    return (
-        <Menu inverted style={{ marginTop: "10px" }}>
-            <Link route='/'>
-                <a className='item'>
-                    Movies
+const searchStyle = {
+    'background': '#0D0D0D'
+}
+
+class MenuBar extends Component {
+    state = {
+        isLoading: false,
+        results: [],
+        value: ''
+    }
+
+    handleSearchChange = (e, { value }) => {
+        if (value) {
+            this.setState({ isLoading: true, value: value })
+
+            const encoded_value = encodeURIComponent(value)
+            const API_KEY = 'cb4a1628811db83e67e78e9eda59ef8b'
+
+            const searchURL =
+                `https://api.themoviedb.org/3/search/movie?query=${encoded_value}&api_key=${API_KEY}&language=en-US&page=1&include_adult=false`
+
+            fetch(searchURL)
+                .then(res => res.json())
+                .then(result => {
+
+                    let results = []
+
+                    if (result.results) {
+                        const topFiveResults = result.results.slice(0, 5)
+                        results = topFiveResults.map((movie) => {
+                            return {
+                                "key": movie.id,
+                                "id": movie.id,
+                                "title": movie.title,
+                                "image": (movie.poster_path ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path : ''),
+                                "description": movie.release_date.slice(0, 4)
+                            }
+                        })
+                    }
+
+                    this.setState({
+                        isLoading: false,
+                        results: results
+                    })
+                })
+        }
+
+        this.setState({ isLoading: false, value: value })
+    }
+
+    render() {
+        return (
+            <Menu inverted style={{ marginTop: "10px" }}>
+                <Link route='/'>
+                    <a className='item'>
+                        Movies
                 </a>
-            </Link>
-            <Menu.Menu position='right'>
-                <Link route='/'>
-                    <a className='item'>
-                        My Movies
-                    </a>
                 </Link>
-                <Link route='/'>
-                    <a className='item'>
-                        MetaMask
+                <Menu.Menu position='right'>
+                    <Search
+                        floated='right'
+                        placeholder='Search any movie...'
+                        loading={this.state.isLoading}
+                        results={this.state.results}
+                        value={this.state.value}
+                        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                            leading: true,
+                        })}
+                        style={searchStyle}
+                    />
+                </Menu.Menu>
+                <Menu.Menu position='right'>
+                    <Link route='/'>
+                        <a className='item'>
+                            My Movies
                     </a>
-                </Link>
-            </Menu.Menu>
-        </Menu>
-    )
+                    </Link>
+                    <Link route='/'>
+                        <a className='item'>
+                            MetaMask
+                    </a>
+                    </Link>
+                </Menu.Menu>
+            </Menu>
+        )
+    }
 }
 
 export default MenuBar
